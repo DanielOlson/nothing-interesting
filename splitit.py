0cecdc460edf53e6cf9863d9241cc40f761ff941
Daniel R. Olson <daniel.olson@me.com>
2021-07-27 14:02:34 -0600

    Added overlap support for splitit

--- splitit.py
+++ splitit.py
@@ -16,8 +16,8 @@ from math import ceil
 
 
 
-if len(sys.argv) != 4:
-	print("Usage: splitit <input_file> <output_name> <symbols_per_file>")
+if len(sys.argv) != 5:
+	print("Usage: splitit <input_file> <output_name> <symbols_per_file> <overlap>")
 	print("Example: splitit input.fa genome 1000")
 	exit()
 
@@ -25,6 +25,7 @@ if len(sys.argv) != 4:
 input_file = sys.argv[1]
 output_name = sys.argv[2]
 seqLength = int(sys.argv[3])
+overlap = int(sys.argv[4])
 
 #Read the input file
 records = list(SeqIO.parse(input_file, "fasta"))
@@ -37,15 +38,23 @@ for r in records:
 	bins = ceil(len(seq) / seqLength)
 
 	for i in range(bins):
+		stop = False
 		start = i * seqLength
+		if i > 0:
+			start = start - overlap
 		end = (i + 1) * seqLength
+		end += overlap
 		if end > len(seq):
 			end = len(seq)
+			stop = True
 
 		newrec = SeqRecord(Seq(seq[start:end]), id=r.id, name=r.name, description=r.description)
 
 		with open(output_name + '_' + str(files_counter) + '.fa', 'w') as output_handle:
 			SeqIO.write(newrec, output_handle, 'fasta')
 		files_counter += 1
+
+		if stop == True:
+			break
 
 	print(bins)
